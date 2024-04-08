60d6ebd21deb30993e00281d7800edd02de889fe
Jennifer Nordell <subdev338@gmail.com>
2024-04-07 20:58:10 -0400

    simple variable and interpolation. Stage 2/5 passes tests.

--- Simple Chat Bot (JavaScript)/task/main.js
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -4,6 +4,6 @@ console.log("Hello! My name is Aid.");
 console.log("I was created in 2020.");
 console.log("Please, remind me your name.");
 
-// use input() + to get the user's output
+const userName = input("Please, remind me your name.")
 
-console.log("What a great name you have, {your_name}!");
+console.log(`What a great name you have, ${userName}!`);

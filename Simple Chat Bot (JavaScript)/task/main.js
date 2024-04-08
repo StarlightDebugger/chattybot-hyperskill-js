1bca4cc1b99d46345f6e36e887a296ead6703064
Jennifer Nordell <subdev338@gmail.com>
2024-04-08 00:46:37 -0400

    tests strange here. Can't change anything before the first comment. Stage 3/5 passing

--- Simple Chat Bot (JavaScript)/task/main.js
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -4,6 +4,16 @@ console.log("Hello! My name is Aid.");
 console.log("I was created in 2020.");
 console.log("Please, remind me your name.");
 
-const userName = input("Please, remind me your name.")
+let name = input();
 
-console.log(`What a great name you have, ${userName}!`);
+console.log("What a great name you have, " + name + "!");
+console.log("Let me guess your age.");
+console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+
+// reading all remainders
+const r3 = input();
+const r5 = input();
+const r7 = input();
+const your_age = (r3 * 70 + r5 * 21 + r7 * 15) % 105;
+
+console.log(`Your age is ${your_age}; that's a good time to start programming!`);

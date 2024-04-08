0e317ccbeb6d6e4874656efefdcd0ec3f4768b46
Jennifer Nordell <subdev338@gmail.com>
2024-04-08 13:16:45 -0400

    simple calculation using input from user. Stage 3/5 passes tests.

--- Simple Chat Bot (JavaScript)/task/main.js
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -1,19 +1,24 @@
 const input = require('sync-input');
 
-console.log("Hello! My name is Aid.");
-console.log("I was created in 2020.");
-console.log("Please, remind me your name.");
+console.log("Hello! My name is Aid.")
+console.log("I was created in 2020.")
+console.log("Please, remind me your name.")
 
-let name = input();
+let name = input()
 
-console.log("What a great name you have, " + name + "!");
-console.log("Let me guess your age.");
-console.log("Enter remainders of dividing your age by 3, 5 and 7.");
+console.log("What a great name you have, " + name + "!")
+console.log("Let me guess your age.")
+console.log("Enter remainders of dividing your age by 3, 5 and 7.")
 
-// reading all remainders
-const r3 = input();
-const r5 = input();
-const r7 = input();
-const your_age = (r3 * 70 + r5 * 21 + r7 * 15) % 105;
+let rem3 = Number(input());
+let rem5 = Number(input());
+let rem7 = Number(input());
 
-console.log(`Your age is ${your_age}; that's a good time to start programming!`);
+let age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+console.log("Your age is " + age + "; that's a good time to start programming!");
+console.log("Now I will prove to you that I can count to any number you want.");
+
+// read a number and count to it here
+
+console.log("Completed, have a nice day!");

a805620c35550af7766eb4b27511eb3f743a498c
Jennifer Nordell <subdev338@gmail.com>
2024-04-07 20:56:00 -0400

    begin stage 2

--- Simple Chat Bot (JavaScript)/task/main.js
+++ Simple Chat Bot (JavaScript)/task/main.js
@@ -1,2 +1,9 @@
-console.log("Hello! My name is Aid");
-console.log("I was created in 2024");
+const input = require('sync-input');
+
+console.log("Hello! My name is Aid.");
+console.log("I was created in 2020.");
+console.log("Please, remind me your name.");
+
+// use input() + to get the user's output
+
+console.log("What a great name you have, {your_name}!");
